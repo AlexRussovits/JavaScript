996f082e5d572d250c9936ea7dfe4f48d85824f1
AlexRussovits <sasikrus2002@gmail.com>
2019-11-04 20:10:15 +0200

    Условные операторы

---
+++ Basic/ConditionalOperators/script8.js
@@ -0,0 +1,31 @@
+var carName = 'Ford'
+var carYear = 2008
+var currentYear = 2019
+var carAge = currentYear - carYear
+
+// если возраст машины меньше пяти лет ...
+// если возраст машины больше или равно 5 лет 
+// и меньше или равно 10 лет ...
+// иначе другое сообщение
+
+if (carAge < 5) {
+    console.log(carName + ' младше 5 лет')
+}
+
+else if (carAge >= 5 && carAge <= 10) {
+    console.log(carName + ' больше или равен 5 годам или меньше или равен 10 годам')
+}
+
+else {
+    console.log('Возраст ' + carName + ' равняется ' + carAge + ' годам ')
+}
+
+// 0, null, undefined, '', NaN(Not a Number)  - false
+
+if (NaN) {
+    console.log('Значение true')
+}
+
+else {
+    console.log('Значение false')
+}
