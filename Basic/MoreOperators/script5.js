84ca0af524f7058fbb68e6828d7f2c8ef02ba389
AlexRussovits <sasikrus2002@gmail.com>
2019-11-04 16:16:38 +0200

    Precedence and Others

---
+++ Basic/MoreOperators/script5.js
@@ -0,0 +1,15 @@
+console.log('5 % 2:', 5 % 2)
+console.log('8 % 3:', 8 % 3)
+console.log('15 % 5:', 15 % 5)
+
+var i = 1 
+i++    //i = i + 1  i += 1
+console.log('i', i)
+i-- //i = i - 1  i -= 1
+console.log('i', i)
+i += 3 //i = i + 3 Используется для сложения, вычитания, деления, умножения
+console.log('i', i)
+
+console.log('i', ++i) // i++
+
+console.log('i', --i) // i--
