f18045cc799b4642cefb8ea45ecae59f3f2d9866
AlexRussovits <sasikrus2002@gmail.com>
2019-11-04 15:09:55 +0200

    Variables and Others

---
+++ Basic/TypesofData/script2.js
@@ -0,0 +1,25 @@
+/*
+    Number
+    String
+    Boolean 
+    Object
+    Null
+    Undefined
+*/
+
+var number = 42
+var string = 'Message'
+var isTrue = true // может быть значение false
+var obj = {a: 1}
+var nothing = null
+var undef = undefined
+
+console.log(number)
+console.log(string)
+console.log(isTrue)
+console.log(obj)
+console.log(nothing)
+console.log(undef)
+
+console.log(typeof number)
+console.log(typeof isTrue)
---
+++ Basic/Variables/script.js
@@ -0,0 +1,17 @@
+var message = 'Hello world!'
+var Message = 'Yeah, boyz'
+var helloMessage = 'Hello again!'
+
+var $ = 'Dollar'
+var _ = 'Underscore'
+
+var message2 = 'Hi'
+var message3 = "Javascript, yeah boiii"
+
+console.log(message)
+console.log(Message)
+console.log(helloMessage)
+console.log($)
+console.log(_)
+console.log(message2)
+console.log(message3)
