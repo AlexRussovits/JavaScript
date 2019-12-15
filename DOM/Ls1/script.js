969b99a8125865dd6995d677beb1a56e4011adc5
AlexRussovits <sasikrus2002@gmail.com>
2019-12-15 19:14:15 +0200

    Full Learning DOM

--- DOM/Ls1/script.js
+++ DOM/Ls1/script.js
@@ -28,5 +28,21 @@ var oldHref = a.getAttribute('href')
 a.setAttribute('href', 'https://ya.ru')
 a.textContent = 'Yandex'
 
+//console.log(a.attributes);
 
-console.log(a.attributes);
+
+// Классы
+var box1 = document.querySelector('#box1')
+var box2 = document.querySelector('#box2')
+
+box1.classList.add('red')
+
+var hasClass = box2.classList.contains('blue')
+console.log(hasClass)
+
+if(hasClass) {
+    box2.classList.remove('blue')
+}else{
+    box2.classList.add('blue')
+}
+
